35736c102c66cd658e019d8f9ec3d15cbbc244ff
Vincent <rapiiidooo.vincent.ljeune@gmail.com>
2019-09-05 22:46:25 +0200

    release-0.1

--- app/routes/projects.js
+++ app/routes/projects.js
@@ -2,21 +2,37 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
     model() {
-        return fetch('https://api.october.eu/projects').then(function (response) {
-            let listProjects = [];
+        return fetch('https://api.october.eu/projects').then(async function (response) {
             return response.json().then(function (json) {
                 json.projects.forEach(element => {
                     try {
+                        // Add domain to img url
                         if (element.illustration) {
-                            listProjects.push('https://cdn.october.eu/' + element.illustration.url)
+                            element.illustration.url = 'https://cdn.october.eu/' + element.illustration.url;
+                        } else {
+                            console.log('toto');
+                            element.illustration = { url: '/assets/images/confidential.png' };
                         }
+
+                        // Get day alepsed from now
+                        var oneDay = 24*60*60*1000;
+                        var oneHour = 60*60*1000;
+                        var timeElepsed = new Date().getTime() - new Date(element.onlineDate).getTime();
+                        element.onlineDate = Math.round(Math.abs((timeElepsed)/(oneDay)));
+                        if (element.onlineDate === 0) {
+                            element.onlineDate = (Math.round(Math.abs((timeElepsed)/(oneHour))) + 1).toString() + ' hours ago';
+                        } else {
+                            element.onlineDate = element.onlineDate.toString() + ' days ago'
+                        }
+                        
+                        element.rate = element.rate.toFixed(2);
+                        element.amount = element.amount.toLocaleString('en-GB');
                     } catch (error) {
                         console.log(error);
                     }
                 });
-                console.log(listProjects);
-                return listProjects;
+                return json.projects;
             });
         });
-    }
+    },
 });
